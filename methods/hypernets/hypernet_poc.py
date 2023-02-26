b7edb9c8e7ce7a365b85fa230d981bdc3fafdf58
Mateusz Rajski <mateuszrajski@Mateuszs-MacBook-Air.local>
2023-02-26 19:17:55 +0100

    Disable grad clipping

--- methods/hypernets/hypernet_poc.py
+++ methods/hypernets/hypernet_poc.py
@@ -377,7 +377,7 @@ class HyperNetPOC(MetaTemplate):
                                 if(k.split('.')[0] != "target_net_architecture"):
                                     metrics[f"grad_norm/{k}"] = p.grad.abs().mean().item() if p.grad is not None else 0
  
-                        torch.nn.utils.clip_grad_norm_(self.parameters(), 1)
+                        #torch.nn.utils.clip_grad_norm_(self.parameters(), 1)
                         optimizer.step()
 
                 losses.append(loss_sum.item())
